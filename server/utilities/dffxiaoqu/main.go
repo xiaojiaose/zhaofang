e142c608b960b39ed86651a50b57efb0875d52b8
xiaojiaose <mengdeqi@gmail.com>
2025-09-20 17:55:21 +0800

    no message

---
+++ server/utilities/dffxiaoqu/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/flipped-aurora/gin-vue-admin/server/core"
+	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"github.com/flipped-aurora/gin-vue-admin/server/initialize"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/system"
+	"go.uber.org/zap"
+	"os"
+	"strings"
+)
+
+func Init() {
+	global.GVA_VP = core.Viper() // 初始化Viper
+	initialize.OtherInit()
+	global.GVA_LOG = core.Zap() // 初始化zap日志库
+	zap.ReplaceGlobals(global.GVA_LOG)
+	global.GVA_DB = initialize.Gorm() // gorm连接数据库
+	global.InitZincSearch(global.GVA_CONFIG.ZincSearch.Url, global.GVA_CONFIG.ZincSearch.Username, global.GVA_CONFIG.ZincSearch.Password)
+}
+
+// 增加小区 脚本，小区手动录入到数据库
+func main() {
+
+	Init()
+	filename := "/Users/xiongda/GolandProjects/zhaofang/server/utilities/dffxiaoqu/no_exsit.txt"
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("无法打开文件:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lineNumber := 1
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		// 处理每一行
+		processLine(line, lineNumber)
+		// 如果是超大文件，可以定期输出进度
+		if lineNumber%20000 == 0 {
+			fmt.Printf("=========> 已处理 %d 行\n", lineNumber)
+		}
+		lineNumber++
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("无法打开文件:", err)
+		return
+	}
+	fmt.Printf("文件读取完成，共 %d 行\n", lineNumber)
+	return
+}
+
+func processLine(line string, lineNumber int) {
+	arr := strings.Split(strings.Replace(line, " ", "", -1), ",")
+	//if len(arr) != 5 {
+	//	fmt.Errorf("lineNumber: %d, line: %s", lineNumber, line)
+	//	return
+	//}
+	xqName := arr[0]
+	//buildingName := arr[2]
+	//unitName := arr[3]
+	//houseName := arr[4]
+
+	var xq system.XiaoQu
+	tx := global.GVA_DB.Model(&system.XiaoQu{}).Where("name = ?", xqName).First(&xq)
+	if tx.Error != nil || xq.CommunityId == 0 {
+		fmt.Printf("++++++ %s, %d\n", xqName, xq.CommunityId)
+		return
+	} else {
+		fmt.Printf("==== %s, %d\n", xqName, xq.CommunityId)
+	}
+
+	//xiaoquId := xq.CommunityId
+	////err := global.GVA_DB.Exec("insert into dict_building (community_id, building_open_id, encrypt_building_name) values (?, ?, ?)",
+	////	xiaoquId, uuid.New().String(), buildingName).Error
+	//
+	//var building house.DictBuilding
+	//err := global.GVA_DB.Model(&house.DictBuilding{}).Where("community_id = ? and encrypt_building_name = ?", xiaoquId, buildingName).Find(&building).Error
+	//if err != nil {
+	//	fmt.Printf("find building 错误: %v\n", tx.Error)
+	//	return
+	//}
+	//if building.ID == 0 {
+	//	building = house.DictBuilding{
+	//		CommunityID:         int(xiaoquId),
+	//		BuildingOpenID:      uuid.New().String(),
+	//		EncryptBuildingName: buildingName,
+	//		CreatedAt:           time.Now(),
+	//	}
+	//	global.GVA_DB.Model(&house.DictBuilding{}).Create(&building)
+	//}
+	//
+	//var unit house.DictUnit
+	//err = global.GVA_DB.Model(&house.DictUnit{}).Where("building_open_id = ? and encrypt_unit_name = ?", building.BuildingOpenID, unitName).Find(&unit).Error
+	//if err != nil {
+	//	fmt.Printf("find unit 错误: %v\n", tx.Error)
+	//	return
+	//}
+	//if unit.ID == 0 {
+	//	unit = house.DictUnit{
+	//		BuildingOpenID:  building.BuildingOpenID,
+	//		UnitOpenID:      uuid.New().String(),
+	//		EncryptUnitName: unitName,
+	//		CreatedAt:       time.Now(),
+	//	}
+	//	global.GVA_DB.Model(&house.DictUnit{}).Create(&unit)
+	//}
+	//
+	//err = global.GVA_DB.Exec("insert into dict_house (house_open_id, encrypt_house_name, unit_open_id, created_at) values (?, ?, ?, ?)", uuid.New().String(), houseName, unit.UnitOpenID, time.Now()).Error
+	//if err != nil {
+	//	global.GVA_LOG.Error("插入失败", zap.Error(err))
+	//}
+}
--- server/utilities/dict/main.go
+++ server/utilities/dict/main.go
@@ -9,6 +9,7 @@ import (
 	"go.uber.org/zap"
 	"os"
 	"strings"
+	"time"
 )
 
 func Init() {
@@ -22,7 +23,7 @@ func Init() {
 func main() {
 
 	Init()
-	filename := "/Users/xiongda/GolandProjects/zhaofang/server/utilities/dict/output.txt"
+	filename := "/opt/zhaofang/output.txt"
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Println("无法打开文件:", err)
@@ -41,6 +42,7 @@ func main() {
 		if lineNumber%20000 == 0 {
 			fmt.Printf("=========> 已处理 %d 行\n", lineNumber)
 		}
+		lineNumber++
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Println("无法打开文件:", err)
@@ -52,33 +54,33 @@ func main() {
 
 func processLine(line string, lineNumber int) {
 	if strings.Contains(line, "encryptHouseName") {
-		//arr := strings.Split(strings.Replace(line, " ", "", -1), ",")
-		//houseName := strings.Split(arr[3], ":")[1]
-		//houseId := strings.Split(arr[2], ":")[1]
-		//unitId := strings.Split(arr[1], ":")[1]
-		//
-		////fmt.Printf("lineNumber: %d, houseName: %s, houseId: %s, unitId: %s\n", lineNumber, houseName, houseId, unitId)
-		//err := global.GVA_DB.Exec("insert into dict_house (house_open_id, encrypt_house_name, unit_open_id) values (?, ?, ?)", houseId, houseName, unitId).Error
-		//if err != nil {
-		//	global.GVA_LOG.Error("插入失败", zap.Error(err))
-		//}
+		arr := strings.Split(strings.Replace(line, " ", "", -1), ",")
+		houseName := strings.Split(arr[3], ":")[1]
+		houseId := strings.Split(arr[2], ":")[1]
+		unitId := strings.Split(arr[1], ":")[1]
+
+		//fmt.Printf("lineNumber: %d, houseName: %s, houseId: %s, unitId: %s\n", lineNumber, houseName, houseId, unitId)
+		err := global.GVA_DB.Exec("insert into dict_house (house_open_id, encrypt_house_name, unit_open_id, created_at) values (?, ?, ?, ?)", houseId, houseName, unitId, time.Now()).Error
+		if err != nil {
+			global.GVA_LOG.Error("插入失败", zap.Error(err))
+		}
 	} else if strings.Contains(line, "encryptUnitName") {
-		//arr := strings.Split(strings.Replace(line, " ", "", -1), ",")
-		//unitName := strings.Split(arr[3], ":")[1]
-		//unitId := strings.Split(arr[2], ":")[1]
-		//buildingId := strings.Split(arr[1], ":")[1]
-		//err := global.GVA_DB.Exec("insert into dict_unit (building_open_id, unit_open_id, encrypt_unit_name) values (?, ?, ?)", buildingId, unitId, unitName).Error
-		//if err != nil {
-		//	global.GVA_LOG.Error("插入失败", zap.Error(err))
-		//}
+		arr := strings.Split(strings.Replace(line, " ", "", -1), ",")
+		unitName := strings.Split(arr[3], ":")[1]
+		unitId := strings.Split(arr[2], ":")[1]
+		buildingId := strings.Split(arr[1], ":")[1]
+		err := global.GVA_DB.Exec("insert into dict_unit (building_open_id, unit_open_id, encrypt_unit_name, created_at) values (?, ?, ?, ?)", buildingId, unitId, unitName, time.Now()).Error
+		if err != nil {
+			global.GVA_LOG.Error("插入失败", zap.Error(err))
+		}
 	} else if strings.Contains(line, "encryptBuildingName") {
-		//arr := strings.Split(strings.Replace(line, " ", "", -1), ",")
-		//buildingName := strings.Split(arr[2], ":")[1]
-		//buildingId := strings.Split(arr[1], ":")[1]
-		//xiaoquId := strings.Split(arr[0], ":")[1]
-		//err := global.GVA_DB.Exec("insert into dict_building (community_id, building_open_id, encrypt_building_name) values (?, ?, ?)", xiaoquId, buildingId, buildingName).Error
-		//if err != nil {
-		//	global.GVA_LOG.Error("插入失败", zap.Error(err))
-		//}
+		arr := strings.Split(strings.Replace(line, " ", "", -1), ",")
+		buildingName := strings.Split(arr[2], ":")[1]
+		buildingId := strings.Split(arr[1], ":")[1]
+		xiaoquId := strings.Split(arr[0], ":")[1]
+		err := global.GVA_DB.Exec("insert into dict_building (community_id, building_open_id, encrypt_building_name, created_at) values (?, ?, ?, ?)", xiaoquId, buildingId, buildingName, time.Now()).Error
+		if err != nil {
+			global.GVA_LOG.Error("插入失败", zap.Error(err))
+		}
 	}
 }
